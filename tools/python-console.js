ea5b66bff69f32071752ab21a964d71d4978c830
menwst <77152418+menwst@users.noreply.github.com>
2023-01-28 00:17:37 +0000

    added styling and formatting with codemirror

--- tools/python-console.js
+++ tools/python-console.js
@@ -1,22 +1,35 @@
 languagePluginLoader.then(() => {
-    const input = document.getElementById("code-input");
     const runButton = document.getElementById("run-button");
+    const textarea = document.getElementById("code-input");
+    const editor = CodeMirror.fromTextArea(textarea, {
+        mode: "python",
+        lineNumbers: true,
+        theme: "default",
+    });
+    editor.getWrapperElement().style.height = "400px";
+    editor.getWrapperElement().style.marginTop = "10px";
+    editor.getWrapperElement().style.marginBottom = "10px";
+    editor.setValue(`import random
 
+def roll_dice():
+ return random.randint(1, 6)
+
+print("You rolled a " + str(roll_dice()))`);
     let callback = function(output) {
         document.getElementById("terminal-output").innerText = output;
     }
     pyodide.globals.callback = callback;
-
+    
     runButton.addEventListener("click", () => {
-        const code = input.value;
+        // const code = input.value;
+        const code = editor.getValue();
         try {
             pyodide.runPython(`
-                import io
-                import sys
-                sys.stdout = io.StringIO()
-                ${code}
-                callback(sys.stdout.getvalue())
-            `);
+import io
+import sys
+sys.stdout = io.StringIO()
+${code}
+callback(sys.stdout.getvalue())`);
         } catch (error) {
             document.getElementById("terminal-output").innerText = error;
         }
